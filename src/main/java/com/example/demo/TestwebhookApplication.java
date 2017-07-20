b07b65de5e4fd89617c88455f0169576476304e5
Alex Pogrebenniy <pogrebenniy.alexandr@gmail.com>
2017-07-20 17:17:20 +0300

    First cfdsfsd

--- src/main/java/com/example/demo/TestwebhookApplication.java
+++ src/main/java/com/example/demo/TestwebhookApplication.java
@@ -6,12 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
 @Controller
 public class TestwebhookApplication {
 
+	private List<Map<String, String>> mapList = new ArrayList<>();
 
 	@RequestMapping("/")
 	@ResponseBody
@@ -19,9 +22,16 @@ public class TestwebhookApplication {
 		return "Hello World and kek!";
 	}
 
+	@RequestMapping("/requests")
+	@ResponseBody
+	List<Map<String, String>> homerequests() {
+		return mapList;
+	}
+
 	@RequestMapping("/webhook")
 	@ResponseBody
 	String webHookEndPoint(Map<String, String> request){
+		mapList.add(request);
 		String s = request.get("hub.challenge");
 		return s;
 	}
