c3348ea5d836dc7df87f77ff06f5083a3c6041e9
Alex Pogrebenniy <pogrebenniy.alexandr@gmail.com>
2017-07-20 17:11:14 +0300

    Start webHook

--- src/main/java/com/example/demo/TestwebhookApplication.java
+++ src/main/java/com/example/demo/TestwebhookApplication.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+
 @SpringBootApplication
 @Controller
 public class TestwebhookApplication {
@@ -15,6 +17,13 @@ public class TestwebhookApplication {
 	@ResponseBody
 	String home() {
 		return "Hello World and kek!";
+	}
+
+	@RequestMapping("/webhook")
+	@ResponseBody
+	String webHookEndPoint(Map<String, String> request){
+		String s = request.get("hub.challenge");
+		return s;
 	}
 
 	public static void main(String[] args) {
