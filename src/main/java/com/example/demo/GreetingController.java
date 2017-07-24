8ea6547797e25bade523f70fff28c37ec7959da6
Alex Pogrebenniy <pogrebenniy.alexandr@gmail.com>
2017-07-24 13:54:19 +0300

    fsdlfmsk fsdf dfs afdfsds

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import com.example.demo.entity.Greeting;
 import com.example.demo.entity.HelloMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -15,7 +14,7 @@ public class GreetingController {
 
 
     @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
+    //@SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
         Thread.sleep(3000); // simulated delay
         return new Greeting("Hello, " + message.getName() + "!");
