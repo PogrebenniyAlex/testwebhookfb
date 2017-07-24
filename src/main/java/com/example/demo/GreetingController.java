64548413ba1dca51994b4bed41c50cd74e9cae34
Alex Pogrebenniy <pogrebenniy.alexandr@gmail.com>
2017-07-24 14:01:18 +0300

    Add web socket webhook message sdfsd

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.example.demo.entity.Greeting;
 import com.example.demo.entity.HelloMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -14,9 +15,9 @@ public class GreetingController {
 
 
     @MessageMapping("/hello")
-    //@SendTo("/topic/greetings")
+    @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(3000); // simulated delay
+        //Thread.sleep(3000); // simulated delay
         return new Greeting("Hello, " + message.getName() + "!");
     }
 
